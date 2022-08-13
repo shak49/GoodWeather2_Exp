d2a953f0fc65a322c8c4f65f3000d42da5e6b0c1
Shak49 <shakfeizi@gmali.com>
2022-08-13 13:16:57 -0500

    model impelemented

--- GoodWeather2_Exp/Models/Weather.swift
+++ GoodWeather2_Exp/Models/Weather.swift
@@ -8,9 +8,67 @@
 import Foundation
 
 struct WeatherResponse: Decodable {
+    let name: String
+    let weather: Weather
+    let icons: [WeatherIcon]
+    let sys: Sys
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ResponseKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.icons = try container.decode([WeatherIcon].self, forKey: .icon)
+        self.sys = try container.decode(Sys.self, forKey: .sys)
+        let weatherContainer = try container.nestedContainer(keyedBy: WeatherKeys.self, forKey: .weather)
+        let tempreture = try weatherContainer.decode(Double.self, forKey: .tempreture)
+        weather = Weather(city: name, tempreture: tempreture, icon: self.icons.first!.icon, sunrise: self.sys.sunrise, sunset: self.sys.sunset)
+    }
     
 }
 
-struct Weather: Decodable {
+extension WeatherResponse {
+    private enum ResponseKeys: String, CodingKey {
+        case name
+        case weather = "main"
+        case icon = "weather"
+        case sys = "sys"
+    }
     
+    enum WeatherKeys: String, CodingKey {
+        case tempreture = "temp"
+    }
+}
+
+
+struct Weather: Decodable {
+    let city: String
+    let tempreture: Double
+    let icon: String
+    let sunrise: Date
+    let sunset: Date
+}
+
+struct WeatherIcon: Decodable {
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Sys: Decodable {
+    let sunrise: Date
+    let sunset: Date
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: SysKeys.self)
+        let sunriseTimeInterval = try container.decode(Int32.self, forKey: .sunrise)
+        let sunsetTimeInterval = try container.decode(Int32.self, forKey: .sunset)
+        self.sunrise = Date(timeIntervalSince1970: TimeInterval(sunriseTimeInterval))
+        self.sunset = Date(timeIntervalSince1970: TimeInterval(sunsetTimeInterval))
+    }
+}
+
+extension Sys {
+    private enum SysKeys: String, CodingKey {
+        case sunrise = "sunrise"
+        case sunset = "sunset"
+    }
 }
--- GoodWeather2_Exp/Utils/Constants.swift
+++ GoodWeather2_Exp/Utils/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Constants {
     struct Urls {
         static func weatherByCity(city: String) -> URL? {
-            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=ef0fd9866ca027e0dca474cee84c53be")
+            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=a0daaf29293c8c70eceb28ebc8a6520d")
         }
         
         static func weatherUrlAsStringByIcon(icon: String) -> String {
